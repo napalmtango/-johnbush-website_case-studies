050d71bf82276f74fb052efb204e66298053216b
John Bush <142678424+napalmtango@users.noreply.github.com>
2023-12-08 22:07:22 -0800

    Rough layout of modal and modal overlay

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-console.log('//External script file being read');
+console.log('//External script file being read from scripts/script.js');
+
+$('#open').click(function () {
+  console.log('Modal button clicked');
+  $('.modal').addClass('active');
+});
+
+$('#close').click(function () {
+  console.log('Modal close button clicked');
+  $('.modal').removeClass('active');
+});
